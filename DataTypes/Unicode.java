87402d0ba981c8912d1485080a3051cf21906ab0
K0MPLEXWorksTogether <abhirammangipudi03@gmail.com>
2024-11-15 12:14:01 +0530

    Small Changes To File Names

--- DataTypes/Unicode.java
+++ DataTypes/Unicode.java
@@ -1,4 +1,4 @@
-class Telugu {
+class Unicode {
     public static void main(String[] args) {
         for(char character = 0x0370; character <= 0x03FF; character++){
             System.out.printf("%c ", character);
